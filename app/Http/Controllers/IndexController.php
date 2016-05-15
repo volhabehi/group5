be172b9a5a8b26ce797b9baf1ac6205fe4eec17d
belhard <ldarik2l@gmail.com>
2016-05-15 18:16:05 +0300

    db select WIP

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use Debugbar;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
 class IndexController extends Controller
 {
     public function index()
     {
+    	$t = DB::table('tests')->where('id', '>', 3)->orWhere('salary', '<', 1234)->get();
+
+    	Debugbar::info($t);
     	return view('index.index');
     }
 }
